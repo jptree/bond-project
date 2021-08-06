847ebaeb9258a31688db6a3cf5b2c96603a3f718
Jason <jasonfpetri@outlook.com>
2021-08-05 22:54:58 -0400

    Initial commit!

---
+++ main.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+# D:\Python\Python38\Scripts\streamlit run main.py
+
+
+if __name__ == "__main__":
+    st.write("""
+    # My first app
+    Hello *world!*
+    """)
+
+    st.line_chart([1, 2, 3, 2, 4, 5, 1, 9])
